b66fc1eb4f69e97b2223ccaf6546f250b616915c
Dima Trembovetski <dmitri.trembovetski@bluescape.com>
2024-08-14 09:30:02 -0700

    before interview, testing stuffs

--- app-esm-jest/src/main/interview.ts
+++ app-esm-jest/src/main/interview.ts
@@ -1,18 +1,22 @@
+import {PriorityQueue} from '@datastructures-js/priority-queue';
 import moment from 'moment';
 
 interface Client {
-  expiredAtMs: number;
+  expiresAtMs: number;
   tokens: number;
 }
 
 class RateLimiter1 {
-  private clients = new Map<string, Client>();
+  private readonly clients = new Map<string, Client>();
+
   constructor(private readonly windowMs: number, private readonly tokensPerWindow: number) {}
 
   checkIfAllowed(key: string, timestamp: number = Date.now()) {
+    // TODO: clean expired client records
+
     let client = this.clients.get(key);
-    if (!client || client.expiredAtMs < timestamp) {
-      client = {expiredAtMs: timestamp + this.windowMs, tokens: this.tokensPerWindow};
+    if (!client || client.expiresAtMs < timestamp) {
+      client = {expiresAtMs: timestamp + this.windowMs, tokens: this.tokensPerWindow};
       this.clients.set(key, client);
     }
     if (client.tokens > 0) {
@@ -22,8 +26,15 @@ class RateLimiter1 {
     return false;
   }
 }
+
 class RateLimiter {
-  constructor(private ratePerSecond: number, private tokens: number = 0, private lastUpdateTsMs = Date.now()) {
+  constructor(
+    private ratePerSecond: number,
+    private burstLimit: number,
+    private tokens: number = 0,
+    private lastUpdateTsMs = Date.now(),
+  ) {
+    // adjusts tokens if needed (to be less than burstLimit)
     this.update(lastUpdateTsMs);
   }
 
@@ -36,13 +47,43 @@ class RateLimiter {
     const grant = Math.floor(delta / tokensPerSecond);
     const surplus = delta % tokensPerSecond;
     this.lastUpdateTsMs = currentTsMs - surplus;
-    this.tokens += grant;
+    this.tokens = Math.min(this.burstLimit, this.tokens + grant);
   }
 
-  public allow(requestedCount: number) {}
+  public allow(currentTsMs = Date.now()): boolean {
+    this.update(currentTsMs);
+    if (this.tokens > 0) {
+      this.tokens--;
+      return true;
+    }
+    return false;
+  }
 }
 
+function doStuffs({a = <number>1, b = <string>'what', c = <boolean>true} = {}) {}
+
 export function doTheThing(param: string) {
+  const timer = setTimeout(() => {}, 100);
+  clearTimeout(timer);
+
+  doStuffs({a: 2});
+  type ReadonlyUserInfo = readonly [string, number];
+  const stuff: ReadonlyUserInfo = ['asd', 11];
+  // stuff[0] = 1;
+  type UUU = {
+    a: string;
+    stuff: () => string; // { return '';};
+  };
+  let bb: UUU = {
+    a: 'asdf',
+    stuff() {
+      return '';
+    },
+  };
+
+  const q = new PriorityQueue((a: string, b: string) => (a > b ? 1 : a < b ? -1 : 0));
+  q.push('asdf');
+  const f = moment().utcOffset();
   const mom = moment();
   const day = mom.dayOfYear();
   return 'hello ' + param + day;
